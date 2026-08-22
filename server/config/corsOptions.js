b8f2de125f0ed97eecc13b6b0a3eb30da62f1fc1
Uttam-Dev16 <kumaruttam72092>
2026-08-22 08:44:28 +0530

    fix CORS configuration for production frontend

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,15 +1,17 @@
 const allowedOrigins = [
   process.env.CLIENT_URL,
+  "https://student-class-test-system-1.onrender.com",
   "http://localhost:3000",
   "http://localhost:5173",
 ].filter(Boolean);
 
 const corsOptions = {
   origin: (origin, callback) => {
-    // Postman/server-to-server requests ke liye
+    // Postman / server-to-server requests
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
+      console.log("Blocked by CORS:", origin);
       callback(new Error("Not allowed by CORS"));
     }
   },
